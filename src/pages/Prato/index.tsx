166a1e1cefbb39c5eb7380b693cb5ef27c8fe559
Leonardo Luz <leonardoluz10@hotmail.com>
2024-08-09 20:34:24 -0300

    feat: :bookmark: Foram refatorado as rotas para adaptar o header no NotFound e refatorado componente Prato. Curso concluído.

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,38 +1,46 @@
 import styles from './Prato.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaBase from 'components/PaginaBase';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato){
-    return (<></>);
+  if (!prato) {
+    return (<NotFound />);
   }
 
   return (
-    <>
-      <button
-        onClick={() => { navigate(-1); }}
-        className={styles.voltar}
-      >
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-          <TagsPrato {...prato}/>
-        </div>
-      </section>
-    </>
+    <Routes>
+      <Route path='*' element={<PaginaBase />} >
+        <Route index element={
+          <>
+            <button
+              onClick={() => { navigate(-1); }}
+              className={styles.voltar}
+            >
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.titulo}>
+                {prato.title}
+              </h1>
+              <div className={styles.imagem}>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <div className={styles.conteudo}>
+                <p className={styles.conteudo__descricao}>
+                  {prato.description}
+                </p>
+                <TagsPrato {...prato} />
+              </div>
+            </section>
+          </>
+        }/>
+      </Route>
+    </Routes>
   );
 }
